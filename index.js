276f643c0f40e1751b6e6288108b09713c88d769
Hexadecimal <11241929+704009905@users.noreply.github.com>
2024-08-06 23:13:20 +0800

    InitCommit

---
+++ index.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const io = new Server(createServer(app).listen(8120), {connectionStateRecovery: {}});
+
+app.get("/", (request, reseponse) => {
+  reseponse.end(fs.readFileSync("./index.html"));
+});
+app.get("/js/mpegts.js", (request, reseponse) => {
+  reseponse.end(fs.readFileSync("./node_modules/mpegts.js/dist/mpegts.js"));
+});
+
+io.on('connection', (socket) => {
+  socket.on('chatMessage', (messagePayload) => {
+    io.emit('chatMessage', messagePayload);
+  });
+});
